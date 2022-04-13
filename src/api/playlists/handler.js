e59e7ad70142147c1206d00801c18af5051774c6
Mumtazah Azma <74398823+mumtazah@users.noreply.github.com>
2022-04-13 08:57:27 +0700

    Update handler.js

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -83,7 +83,7 @@ class PlaylistsHandler {
 
   async deletePlaylistByIdHandler(request, h) {
     try {
-      const { id: playlistId } = request.params;
+      const { playlistId } = request.params;
       const { id: credentialId } = request.auth.credentials;
       await this._service.verifyPlaylistOwner(playlistId, credentialId);
       await this._service.deletePlaylistById(playlistId);
@@ -192,7 +192,7 @@ class PlaylistsHandler {
   async deleteSongByIdHandler(request, h) {
     try {
       this._validator.validateSongPayload(request.payload);
-      const { id: playlistId } = request.params;
+      const { playlistId } = request.params;
       const { songId } = request.payload;
       const { id: credentialId } = request.auth.credentials;
 
